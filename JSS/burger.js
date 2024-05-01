ef7c98755dbddd57e4c4916c6b16d76c7401a2a2
Guillermo Piccardo <g.e.piccardo@gmail.com>
2024-05-01 13:24:55 -0300

    santeV3-1.5

--- JSS/burger.js
+++ JSS/burger.js
@@ -40,3 +40,33 @@ links.forEach((link) => {
     })
 });
 
+//Efecto imagenes sobreNosotros
+
+const imagen1 = document.getElementById('imagen1')
+const imagen2 = document.getElementById('imagen2')
+
+const cargarImagen = (entradas, observador) => {
+    console.log('ejecuta funcion');
+    entradas.forEach((entrada) =>{
+        console.log('empieza el IF');
+        if(entrada.isIntersecting){
+            console.log('se muestra la imagen');
+            entrada.target.classList.add('visible')
+        }else{
+            // entrada.target.classList.remove('visible')
+
+        }
+    })
+
+}
+
+const observador = new IntersectionObserver(cargarImagen, {
+
+    root: null,
+    rootMargin: '0px 0px 0px 0px',
+    threshold: 0.3
+
+})
+
+observador.observe(imagen1)
+observador.observe(imagen2)
